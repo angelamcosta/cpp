170cc76f170303a912abd1f438e76c9427ca8c66
Angela Lima <angelamcostalima@icloud.com>
2023-05-28 13:06:08 +0100

    :sparkles: add megaphone exercise to CPP 00

---
+++ CPP 00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 18:40:16 by anlima            #+#    #+#             */
+/*   Updated: 2023/05/28 13:04:17 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int		main(int argc, char **argv) {
+	int	i = 0;
+	if (argc > 1)
+	{
+		while (argv[++i])
+		{
+			int j = -1;
+			while (argv[i][++j])
+			{
+				argv[i][j] = toupper(argv[i][j]);
+				std::cout << argv[i][j];
+			}
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
