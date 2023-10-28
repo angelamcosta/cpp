4dae8697246ff56f96a57633333071d6f389f60c
Angela Lima <angelamcostalima@icloud.com>
2023-10-28 23:43:36 +0100

    🐛 fix `ex01` addContact function

--- CPP 00/ex01/PhoneBook.cpp
+++ CPP 00/ex01/PhoneBook.cpp
@@ -6,21 +6,45 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 19:39:25 by anlima            #+#    #+#             */
-/*   Updated: 2023/05/30 21:42:49 by anlima           ###   ########.fr       */
+/*   Updated: 2023/10/28 23:39:08 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include <string>
 
-void	PhoneBook::addContact(void)
+void PhoneBook::addContact(void)
+{
+	static int count = 0;
+	int i;
+
+	i = count % 8;
+	Contact contact = CreateContact(i);
+	this->phone_book_[i] = contact;
+	std::cout << GREEN << "Contact '" << contact.getName() << "' added successfully!" << CLEAR << std::endl;
+	std::cout << GREEN << "Contact '[" << i << "]': " << this->phone_book_[i].getName() << CLEAR << std::endl;
+	count++;
+}
+
+void PhoneBook::promptMenu(void)
+{
+	std::cout << YELLOW << "\u2022 Type ADD to add a new contact;" << std::endl;
+	std::cout << YELLOW << "\u2022 Type SEARCH to search for a contact;" << std::endl;
+	std::cout << YELLOW << "\u2022 Type EXIT to exit your phone book.\n‣ ";
+}
+
+void PhoneBook::searchContact(void)
+{
+	std::cout << PINK << "You chose the SEARCH option" << CLEAR << std::endl;
+}
+
+Contact PhoneBook::CreateContact(int i)
 {
 	std::string first_name;
 	std::string last_name;
 	std::string nickname;
 	std::string darkest_secret;
-	long		phone_number;
-	int			i = 0;
+	long phone_number;
 
 	std::cout << PINK << "You chose the ADD option" << CLEAR << std::endl;
 	std::cout << BLUE << "Type the first name ‣ " << CLEAR;
@@ -33,23 +57,6 @@ void	PhoneBook::addContact(void)
 	std::cin >> darkest_secret;
 	std::cout << BLUE << "Type the phone number ‣ " << CLEAR;
 	std::cin >> phone_number;
-
-	while(i < 8 && this->phone_book_[i].getId() != -1)
-		i++;
-	Contact 	contact(i, first_name, last_name, nickname, darkest_secret, phone_number);
-	this->phone_book_[i] = contact;
-	std::cout << GREEN << "Contact '" << first_name << "' added successfully!" << CLEAR << std::endl;
-	std::cout << GREEN << "Contact '[" << i << "]': " << this->phone_book_[i].getName() << CLEAR << std::endl;
-}
-
-void	PhoneBook::searchContact(void)
-{
-	std::cout << PINK << "You chose the SEARCH option" << CLEAR << std::endl;
-}
-
-void	PhoneBook::promptMenu(void)
-{
-	std::cout << YELLOW << "\u2022 Type ADD to add a new contact;" << std::endl;
-	std::cout << YELLOW << "\u2022 Type SEARCH to search for a contact;" << std::endl;
-	std::cout << YELLOW << "\u2022 Type EXIT to exit your phone book.\n‣ ";
+	Contact contact(i, first_name, last_name, nickname, darkest_secret, phone_number);
+	return (contact);
 }
--- CPP 00/ex01/PhoneBook.hpp
+++ CPP 00/ex01/PhoneBook.hpp
@@ -6,29 +6,32 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 19:39:27 by anlima            #+#    #+#             */
-/*   Updated: 2023/05/30 21:29:26 by anlima           ###   ########.fr       */
+/*   Updated: 2023/10/28 23:36:20 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
-# define RED "\033[91;1m"
-# define GREEN "\033[92;1m"
-# define YELLOW "\033[93;1m"
-# define BLUE "\033[94;1m"
-# define PINK "\033[95;1m"
-# define CLEAR "\033[0m"
-# define DECORATION "ᐠ⸜ˎ_ˏ⸝^⸜ˎ_ˏ⸝^⸜ˎ_ˏ⸝ᐟ"
+#define PHONEBOOK_HPP
+#define RED "\033[91;1m"
+#define GREEN "\033[92;1m"
+#define YELLOW "\033[93;1m"
+#define BLUE "\033[94;1m"
+#define PINK "\033[95;1m"
+#define CLEAR "\033[0m"
+#define DECORATION "ᐠ⸜ˎ_ˏ⸝^⸜ˎ_ˏ⸝^⸜ˎ_ˏ⸝ᐟ"
 
-# include "Contact.hpp"
+#include "Contact.hpp"
 
-class PhoneBook {
-	private:
-		Contact	phone_book_[8];
-	public:
-		void	addContact(void);
-		void	searchContact(void);
-		void	promptMenu(void);
+class PhoneBook
+{
+private:
+	Contact phone_book_[8];
+
+public:
+	void addContact(void);
+	void searchContact(void);
+	static void promptMenu(void);
+	static Contact CreateContact(int i);
 };
 
 #endif
