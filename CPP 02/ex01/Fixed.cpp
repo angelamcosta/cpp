55791c36220caf6460ee8529eafd65bf3273c61b
Angela Lima <angelamcostalima@icloud.com>
2023-11-05 15:05:39 +0000

    🎨 reformat `ex01`

--- CPP 02/ex01/Fixed.cpp
+++ CPP 02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:45:37 by anlima            #+#    #+#             */
-/*   Updated: 2023/11/05 14:52:17 by anlima           ###   ########.fr       */
+/*   Updated: 2023/11/05 15:01:10 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ Fixed::Fixed(const int raw) : fixed_(raw << this->bits_) { std::cout << "Int con
 
 Fixed::Fixed(const float raw) : fixed_(roundf(raw * (1 << this->bits_))) { std::cout << "Float constructor called" << std::endl; }
 
-Fixed::Fixed(const Fixed &other) : fixed_(other.fixed_) {
-	std::cout << "Copy constructor called" << std::endl; }
+Fixed::Fixed(const Fixed &other) : fixed_(other.fixed_) { std::cout << "Copy constructor called" << std::endl; }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
@@ -30,10 +29,7 @@ Fixed &Fixed::operator=(const Fixed &other)
 	return (*this);
 }
 
-Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed(void) { std::cout << "Destructor called" << std::endl; }
 
 int Fixed::getRawBits(void) const
 {
@@ -41,22 +37,13 @@ int Fixed::getRawBits(void) const
 	return this->fixed_;
 }
 
-void Fixed::setRawBits(int const raw)
-{
-	this->fixed_ = raw << this->bits_;
-}
+void Fixed::setRawBits(int const raw) { this->fixed_ = raw << this->bits_; }
 
-float Fixed::toFloat(void) const
-{
-	return static_cast<float>(this->fixed_) / (1 << bits_);
-}
+float Fixed::toFloat(void) const { return static_cast<float>(this->fixed_) / (1 << bits_); }
 
-int Fixed::toInt(void) const
-{
-	return this->fixed_ >> this->bits_;
-}
+int Fixed::toInt(void) const { return this->fixed_ >> this->bits_; }
 
-std::ostream & operator<<(std::ostream &os, const Fixed &fixed)
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed)
 {
 	os << fixed.toFloat();
 	return os;
