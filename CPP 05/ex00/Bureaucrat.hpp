6cd2030f991bcfaeea10d3abc7af7cde0f27ebe9
Angela Lima <angelamcostalima@icloud.com>
2024-01-21 21:13:32 +0000

    💡 add comments about exceptions

--- CPP 05/ex00/Bureaucrat.hpp
+++ CPP 05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 13:17:52 by anlima            #+#    #+#             */
-/*   Updated: 2024/01/21 13:51:54 by anlima           ###   ########.fr       */
+/*   Updated: 2024/01/21 20:54:59 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,9 @@ class Bureaucrat
 
 	Bureaucrat &operator=(Bureaucrat const &rhs);
 
+	// we overload the what() function of the std::exception class to return
+	// our error message and the throw() specification specifies that we will not throw
+	// any other exceptions besides the exception type it is associated with
 	class GradeTooHighException : public std::exception {
 		public:
 			virtual const char* what() const throw() { return "Grade too high!"; }
