71a4272828237b502622f9ad07661b847b893a4a
Angela Lima <angelamcostalima@icloud.com>
2024-01-29 14:31:15 +0000

    ✅ add more tests to `main.cpp`

--- CPP 07/ex00/main.cpp
+++ CPP 07/ex00/main.cpp
@@ -3,23 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: anlima <anlima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:07:26 by anlima            #+#    #+#             */
-/*   Updated: 2024/01/24 18:16:52 by anlima           ###   ########.fr       */
+/*   Updated: 2024/01/29 14:30:51 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
+#define CLEAR "\033[0m"
+#define GREEN "\033[32m"
+
+template <typename T> void testActions(T &arg1, T &arg2)
+{
+	std::cout << "The min between arg1 and arg2 is " << GREEN << min(arg1, arg2) << CLEAR << std::endl;
+	std::cout << "The max between arg1 and arg2 is " << GREEN << max(arg1, arg2) << CLEAR << std::endl;
+	std::cout << "Before swap: arg1 = " << GREEN << arg1 << CLEAR << ", arg2 = " << GREEN << arg2 << CLEAR << std::endl;
+	swap(arg1, arg2);
+	std::cout << "After swap: arg1 = " << GREEN << arg1 << CLEAR << ", arg2 = " << GREEN << arg2 << CLEAR << std::endl;
+}
 
 int	main(void)
 {
 	try
 	{
 		int x = 5, y = 10;
-		std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
-		swap(x, y);
-		std::cout << "After swap: x = " << x << ", y = " << y << std::endl;
+		char a = 'a', z = 'z';
+		std::string str1 = "Hello", str2 = "World";
+		
+		testActions(x, y);
+		testActions(a, z);
+		testActions(str1, str2);
 	}
 	catch (const std::exception &e)
 	{
