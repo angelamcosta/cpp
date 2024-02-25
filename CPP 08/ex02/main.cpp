47fe4fdf34717fb64893dcffecb86acab87beea4
Angela Lima <angelamcostalima@icloud.com>
2024-02-25 22:04:05 +0000

    ✅ add tests to `ex02`

--- CPP 08/ex02/main.cpp
+++ CPP 08/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anlima <anlima@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 18:26:09 by anlima            #+#    #+#             */
-/*   Updated: 2024/02/20 13:55:14 by anlima           ###   ########.fr       */
+/*   Updated: 2024/02/25 18:56:18 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,34 @@
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(22);
-	mstack.push(2);
-	mstack.push(737);
-	mstack.push(0);
-	mstack.pop();
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-	std::stack<int> s(mstack);
-	return (0);
+    MutantStack<int> mutantStack;
+
+    mutantStack.push(1);
+    mutantStack.push(2);
+    mutantStack.push(3);
+    mutantStack.push(4);
+    mutantStack.push(5);
+
+    std::cout << "Elements in the stack (using iterators): ";
+    for (MutantStack<int>::iterator it = mutantStack.begin(); it != mutantStack.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    std::cout << "Doubling the elements: ";
+    for (MutantStack<int>::iterator it = mutantStack.begin(); it != mutantStack.end(); ++it)
+    {
+        *it *= 2;
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Popping elements: ";
+    while (!mutantStack.empty())
+    {
+        std::cout << mutantStack.top() << " ";
+        mutantStack.pop();
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
