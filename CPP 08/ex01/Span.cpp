0079ef09f7e1048e8b182293cbdcae218297150e
Angela Lima <angelamcostalima@icloud.com>
2024-03-11 14:00:29 +0000

    ✅ update the tests
    * update the tests (add more)
    * add more exceptions

--- CPP 08/ex01/Span.cpp
+++ CPP 08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 17:08:10 by anlima            #+#    #+#             */
-/*   Updated: 2024/02/20 15:04:15 by anlima           ###   ########.fr       */
+/*   Updated: 2024/03/11 13:34:52 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,17 @@ Span::~Span(){};
 void Span::addNumber(int number)
 {
 	if (this->data.size() == this->size)
-		throw std::out_of_range("The vector is full!");
+		throw std::out_of_range("The vector is full");
 	this->data.push_back(number);
 }
+
 void Span::addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end)
 {
 	if (this->data.size() == this->size)
-		throw std::out_of_range("The vector is full!");
+		throw std::out_of_range("The vector is full");
+	size_t size = std::distance(begin, end);
+	if (this->size < this->data.size() + size)
+		throw std::out_of_range("The vector won't fit all the numbers you're trying to add");
 	data.insert(data.end(), begin, end);
 }
 
--- CPP 08/ex01/main.cpp
+++ CPP 08/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: anlima <anlima@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 17:30:41 by anlima            #+#    #+#             */
-/*   Updated: 2024/02/25 22:03:07 by anlima           ###   ########.fr       */
+/*   Updated: 2024/03/11 13:58:32 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,48 @@
 
 int main(void)
 {
-    try
-    {
-        Span span(7);
+    Span span(10);
 
-        span.addNumber(0);
-        span.addNumber(1);
-        span.addNumber(2);
-        span.addNumber(3);
-
+    try {
+        std::vector<int> numbersToAdd;
+        numbersToAdd.push_back(1);
+        numbersToAdd.push_back(2);
+        numbersToAdd.push_back(3);
+        numbersToAdd.push_back(4);
+        numbersToAdd.push_back(5);
+        span.addNumber(numbersToAdd.begin(), numbersToAdd.end());
+        std::cout << "Numbers added successfully!" << std::endl;
         std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
         std::cout << "Longest span: " << span.longestSpan() << std::endl;
-
-        std::vector<int> additionalNumbers;
-		additionalNumbers.push_back(6);
-		additionalNumbers.push_back(7);
-		additionalNumbers.push_back(8);
-        span.addNumber(additionalNumbers.begin(), additionalNumbers.end());
-        
-        std::cout << "Shortest span after adding more numbers: " << span.shortestSpan() << std::endl;
-        std::cout << "Longest span after adding more numbers: " << span.longestSpan() << std::endl;
-
-        span.addNumber(9);
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
+
+    try {
+        std::vector<int> numbersToAdd;
+        numbersToAdd.push_back(6);
+        numbersToAdd.push_back(7);
+        numbersToAdd.push_back(8);
+        numbersToAdd.push_back(9);
+        numbersToAdd.push_back(10);
+        numbersToAdd.push_back(11);
+        span.addNumber(numbersToAdd.begin(), numbersToAdd.end());
+        std::cout << "Numbers added successfully!" << std::endl;
+        std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << span.longestSpan() << std::endl;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
+
+    // try
+    // {
+    //     span.addNumber(11);
+    //     std::cout << "Numbers added successfully!" << std::endl;
+    //     std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+    //     std::cout << "Longest span: " << span.longestSpan() << std::endl;
+    // } catch (const std::out_of_range& e) {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    // }
+    
     return (0);
 }
